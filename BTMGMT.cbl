7aff3e6e3a22fb219512789015b30078cb105307
Denis_REMACLE <denis.remacle@protonmail.com>
2026-04-28 20:10:23 -0400

    Added code

---
+++ BTMGMT.cbl
@@ -0,0 +1,10 @@
+         IDENTIFICATION DIVISION.
+         PROGRAM-ID. BTMGMT.
+         AUTHOR. DENIS-REMACLE.
+
+         PROCEDURE DIVISION.
+         1000-SEC1 SECTION.
+         1000-PARA.
+            DISPLAY "HELLO WORLD".
+            STOP RUN.
+            
